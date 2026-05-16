d85543e6d18c1bea4c5dfc6bf283fa46414492bd
FANOMEZANTSOA Bien Aimé Louison <hnlouison@gmail.com>
2026-05-16 22:39:00 +0300

    fix docker base image

--- src/main/java/com/example/Triangle.java
+++ src/main/java/com/example/Triangle.java
@@ -1,6 +1,5 @@
 package com.example;
 
-
 public class Triangle {
 
     public static String typeTriangle(int a, int b, int c) {
@@ -9,5 +8,9 @@ public class Triangle {
         if (a == b || b == c || a == c)
             return "Isosceles";
         return "Scalene";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Type du triangle : " + typeTriangle(3, 3, 3));
     }
 }
