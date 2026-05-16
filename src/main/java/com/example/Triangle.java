7b29f297a7ff9ba4f25b49f1059028d3b1c8b53b
FANOMEZANTSOA Bien Aimé Louison <hnlouison@gmail.com>
2026-05-16 21:29:44 +0300

    v1

---
+++ src/main/java/com/example/Triangle.java
@@ -0,0 +1,17 @@
+package main.java.com.example;
+
+
+public class Triangle {
+
+    public static String typeTriangle(int a, int b, int c) {
+        if (a == b && b == c)
+            return "Equilateral";
+        if (a == b || b == c || a == c)
+            return "Isosceles";
+        return "Scalene";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(typeTriangle(3,3,3));
+    }
+}
---
+++ src/test/java/com/example/TriangleTest.java
@@ -0,0 +1,22 @@
+package test.java.com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+
+    @Test
+    public void testEquilateral() {
+        assertEquals("Equilateral", Triangle.typeTriangle(3,3,3));
+    }
+
+    @Test
+    public void testIsosceles() {
+        assertEquals("Isosceles", Triangle.typeTriangle(3,3,4));
+    }
+
+    @Test
+    public void testScalene() {
+        assertEquals("Scalene", Triangle.typeTriangle(3,4,5));
+    }
+}
